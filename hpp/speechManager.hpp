93dfca8bff0d93f33dc0fff72cd19115ffcc0e8f
haley <huanghongweimax@163.com>
2024-06-19 23:52:23 +0800

    演讲比赛类

---
+++ hpp/speechManager.hpp
@@ -0,0 +1,24 @@
+//
+// Created by huang on 2024/6/19.
+//
+
+#ifndef SPEECH_CONTEST_SPEECHMANAGER_H
+#define SPEECH_CONTEST_SPEECHMANAGER_H
+
+#endif //SPEECH_CONTEST_SPEECHMANAGER_H
+
+#pragma once
+#include "iostream"
+
+using namespace std;
+
+//演讲管理类
+class SpeechManager{
+public:
+
+    //构造函数
+    SpeechManager();
+
+    //析构函数
+    ~SpeechManager();
+};
