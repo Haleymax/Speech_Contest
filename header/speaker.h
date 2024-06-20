96ad0bdabec475937a69a0fb3bc8903a01d118fc
haley <huanghongweimax@163.com>
2024-06-20 23:26:09 +0800

    添加选手类中读取数据的函数

--- header/speaker.h
+++ header/speaker.h
@@ -15,7 +15,25 @@ using namespace std;
 
 //选手类
 class Speaker{
-public:
+private:
     string m_Name;   //选手姓名
     double m_Score[2]; //分数（共有两轮）
+
+public:
+    void setName(string name){
+        this->m_Name = name;
+    }
+
+    void setScore(int index , double value){
+        this->m_Score[index] = value;
+    }
+
+    string getName(){
+        return this->m_Name;
+    }
+
+    double getScore(int index){
+        return this->m_Score[index];
+    }
+
 };
