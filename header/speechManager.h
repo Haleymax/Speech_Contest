261244ccf8d03fc637aee1b7e0bfde123d3ca816
haley <huanghongweimax@163.com>
2024-06-20 23:05:31 +0800

    比赛类

---
+++ header/speechManager.h
@@ -0,0 +1,32 @@
+//
+// Created by huang on 2024/6/19.
+//
+
+#ifndef SPEECH_CONTEST_SPEECHMANAGER_H
+#define SPEECH_CONTEST_SPEECHMANAGER_H
+
+#endif //SPEECH_CONTEST_SPEECHMANAGER_H
+
+#pragma once
+#include "iostream"
+
+using namespace std;
+
+//演讲管理类
+class SpeechManager{
+public:
+
+    //构造函数
+    SpeechManager();
+
+    //析构函数
+    ~SpeechManager();
+
+    //显示菜单函数
+    void show_Menu();
+
+    //退出功能
+    void exitSystem();
+};
+
+
