103f7238fbb3a2c0e13be757cf3401f64ea7dcfd
haley <huanghongweimax@163.com>
2024-06-20 23:05:09 +0800

    选手类

---
+++ header/speaker.h
@@ -0,0 +1,21 @@
+//
+// Created by huang on 2024/6/20.
+//
+
+#ifndef SPEECH_CONTEST_SPEAKER_H
+#define SPEECH_CONTEST_SPEAKER_H
+
+#endif //SPEECH_CONTEST_SPEAKER_H
+
+#pragma once
+#include "iostream"
+#include "string"
+using namespace std;
+
+
+//选手类
+class Speaker{
+public:
+    string m_Name;   //选手姓名
+    double m_Score[2]; //分数（共有两轮）
+};
