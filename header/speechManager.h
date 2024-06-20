a634f05ec8146192dc9194c525e0c38bd23ba277
haley <huanghongweimax@163.com>
2024-06-20 23:26:31 +0800

    实现初始化和创建选手的操作

--- header/speechManager.h
+++ header/speechManager.h
@@ -9,6 +9,9 @@
 
 #pragma once
 #include "iostream"
+#include "vector"
+#include "speaker.h"
+#include "map"
 
 using namespace std;
 
@@ -27,6 +30,30 @@ public:
 
     //退出功能
     void exitSystem();
+
+    //初始化属性
+    void initSpeach();
+
+    //初始创建12名选手
+    void createSpeaker();
+
+
+public:
+    //选手容器
+    vector<int> v1;
+
+    //第一轮晋级的容器 6人
+    vector<int> v2;
+
+    //胜利前三名容器
+    vector<int>vVictory;
+
+    //存放选手编号和对应的选手(编号唯一性)
+    map<int , Speaker> m_Speaker;
+
+    //比赛轮次
+    int m_Index = 1;
+
 };
 
 
--- server/speechManager.cpp
+++ server/speechManager.cpp
@@ -7,6 +7,12 @@
 //构造函数
 SpeechManager::SpeechManager() {
 
+    //初始化比赛系统
+    this->initSpeach();
+
+    //创建选手
+    this->createSpeaker();
+
 }
 
 //析构函数
@@ -31,4 +37,38 @@ void SpeechManager::exitSystem() {
     cout << "欢迎下次使用" << endl;
     system("pause");
     exit(EXIT_SUCCESS);
+}
+
+//初始化属性
+void SpeechManager::initSpeach() {
+    this->v1.clear();
+    this->v2.clear();
+    this->vVictory.clear();
+    this->m_Speaker.clear();
+
+    this->m_Index = 1;
+}
+
+//创建12名选手
+void SpeechManager::createSpeaker() {
+    string nameSeed = "ABCDEFGHIJKL";
+    for (int i = 0; i < nameSeed.size(); ++i) {
+        string name = "选手";
+        name += nameSeed[i];
+
+        Speaker sp;
+        sp.setName(name);
+
+        //初始化分数
+        for (int j = 0; j < 2; ++j) {
+            sp.setScore(j,0);
+        }
+
+        //创建12名选手的编号
+        int id = i + 10001;
+        this->v1.push_back(id);
+
+        //将选手和编号放入map中
+        this->m_Speaker.insert(make_pair(id , sp));
+    }
 }
